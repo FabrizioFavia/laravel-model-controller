d159512da780df1c171c1d6381096a9407062368
FabrizioFavia <personal.favia@gmail.com>
2023-06-28 16:18:38 +0200

    completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="mainContainer">
-@foreach ($movies as $movie)
-    <p>{{$movie->title}}</p>
-@endforeach
-</div>
-
+    <div class="mainContainer d-flex flex-wrap mb-3">
+        <div class="container-lg d-flex flex-wrap">
+            @foreach ($movies as $movie)
+                <div class="movieCard me-3 mt-3 p-3 border border-success">
+                    <h4>{{ $movie->title }}</h4>
+                    <p> Titolo originale: {{ $movie->original_title }}</p>
+                    <p>Lingua: {{ $movie->nationality }}</p>
+                    <p>Data d'uscita: {{ $movie->date }}</p>
+                    <p>Voto: {{ $movie->vote }}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
